fb26acf6114da73a00d3317da36e583df61bfff9
Adam Traversy <adamtraversy@gmail.com>
2023-09-24 16:40:51 -0400

    Adds express-ejs-layouts package

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env['PORT'] || 8080;
 const path = require('path');
 
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
@@ -19,17 +22,32 @@ app.use('/services', services);
 
 // Home
 app.get('/', (req, res) => {
-  res.render('index');
+  res.locals = {
+    title: "Klawrzland"
+  };
+  res.render('index', {
+    // additional locals, custom layout, other options
+  });
 });
 
 // Pages
 app.get('/contact', (req, res) => {
+  res.locals = {
+    title: "Contact"
+  };
   res.render('contact');
 });
 
 app.get('/about', (req, res) => {
+  res.locals = {
+    title: "Index"
+  };
   res.render('about');
 });
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
 
 app.listen(port, () => {
   console.log(`Klawrzland is live at http://localhost:${port}`);
